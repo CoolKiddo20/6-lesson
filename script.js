21a3b0e5f6cb9a864cb24e884061a0bc9ca0314a
CoolKiddo20 <161250160+CoolKiddo20@users.noreply.github.com>
2024-06-23 21:26:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let backgroundColor = prompt("Enter background color:");
+    document.body.style.backgroundColor = backgroundColor;
+
+    let textColor = prompt("Enter text's color:");
+    document.body.style.color = textColor;
+});
